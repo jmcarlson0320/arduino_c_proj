2903f08d3113671bc6356b6f118d81239d73fee0
jmcarlson0320 <jmcarlson0320@gmail.com>
2024-09-09 18:03:18 -0700

    Add console module

---
+++ console.h
@@ -0,0 +1,10 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#define CMD_BUF_SIZE 16
+#define PROMPT "> "
+
+void console_init(void);
+void console_update(void);
+
+#endif // CONSOLE_H
--- uart.h
+++ uart.h
@@ -7,13 +7,13 @@
 #define BAUD 9600
 
 #define RX_BUF_SIZE 32
-#define TX_BUF_SIZE 128
+#define TX_BUF_SIZE 64
 
 void uart_init(void);
 void uart_update(void);
 
 bool uart_getc(char *c);
-bool uart_putc(char c);
-uint8_t uart_puts(char *s);
+bool uart_putc(const char c);
+uint8_t uart_puts(const char *s);
 
 #endif // UART_NO_BLOCK_H
